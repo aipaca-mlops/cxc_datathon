566ed6e98c2b51c6d0a99246a14d5d2f331cada2
y2236li <wangqiong@MacBook-Pro-2.lan>
2022-05-14 17:05:07 -0400

    first commit

---
+++ alpaca_model_repo/model/model.py
@@ -0,0 +1,5 @@
+import random
+
+def predict_random_resull(image_data):
+    return random.randint(0, 1)
+
---
+++ alpaca_model_repo/predict.py
@@ -0,0 +1,20 @@
+import os
+from model.model import predict_random_resull
+
+def load_model():
+    # TODO: replace the random model to your owns
+    model = predict_random_resull
+    return model
+
+def run(model) -> list:
+    # You should read image from data folder and return model prediction results
+    # 0 represents non-alpaca
+    # 1 represents alpaca
+    # The return needs to be a list of binaries
+    results = []
+    for f in os.listdir("data"):
+        results.append(model(f))
+    return results
+
+if __name__ == '__main__':
+    print(run(load_model()))
